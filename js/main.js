5db4ea81cbb0fee1a7f99101cd7222a7f5317f25
Marcin Malinowski <mmalinowski265@wp.pl>
2020-12-16 09:48:12 +0100

    new function searchProduct

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const toBuy = [];
 const bought = [];
-const products = [];
+// const products = [];
 const buttonToBuy = document.querySelectorAll('.listElement__button--buy');
-const buttonAddNewTask= document.querySelector('.button__addNewTask');
+const buttonAddNewTask = document.querySelector('.button__addNewTask');
 const buttonClosePopUp = document.querySelector('.closePopUp');
 const popUp = document.querySelector('.popUp');
 const WindowPopUp = document.querySelector('.mainWindowPopUp');
@@ -14,20 +14,42 @@ const catalogButton = document.querySelector('.catalog__button');
 const productDatabase = document.querySelector('.productDatabase');
 const categoryProducts = document.querySelectorAll('.category__productsTitle');
 const categoryProductsShowed = document.querySelectorAll('.category__products');
-// const arrows = document.querySelectorAll('.fas.fa-arrow-up');
+const products = document.querySelectorAll('.category__product');
+const resultSearchProducts = document.querySelector('.result')
 
 
-categoryProducts.forEach(function(categoryProduct) {
-    categoryProduct.addEventListener('click', function() {
-        categoryProducts.forEach(function(categoryProduct) {
+categoryProducts.forEach(function (categoryProduct) {
+    categoryProduct.addEventListener('click', function () {
+        categoryProducts.forEach(function (categoryProduct) {
             categoryProduct.classList.remove('show');
             console.log('zabrało')
         })
         categoryProduct.classList.add('show');
-        console.log('dodało')
-       
+        console.log('dodało');
+        // if(categoryProduct.contains(show))
+
     })
 })
+const searchProduct = (e) => {
+    const searchText = e.target.value.toLowerCase();
+    let tasks = [...products];
+    let taskss = [...products];
+    taskss = taskss.filter(task => task.textContent.toLowerCase().includes(searchText))
+    resultSearchProducts.textContent = "";
+    taskss.forEach((task) => {
+        resultSearchProducts.appendChild(task.cloneNode(task))
+    })
+
+    if (!fieldToSearchProduct.value) {
+        resultSearchProducts.textContent = "";
+    }
+}
+var tablica = [3, 2, 2, 1, 4];
+var kopiaTablicy = tablica.slice();
+kopiaTablicy.sort();
+
+
+fieldToSearchProduct.addEventListener('input', searchProduct)
 
 const ActivePopUp = () => {
     popUp.classList.add('active');
@@ -47,20 +69,25 @@ const ClosePopUp = () => {
 
 const showCatalogProducts = () => {
     productDatabase.classList.toggle('showed');
-    categoryProducts.forEach(function(categoryProduct) {
+    categoryProducts.forEach(function (categoryProduct) {
         categoryProduct.classList.remove('show');
-})};
+    })
+};
 
-const searchProduct = fieldToSearchProduct.value;
+// const searchProduct = fieldToSearchProduct.value;
 
 const onFormSubmit = (e) => {
     e.preventDefault();
 };
 const init = () => {
-form.addEventListener('submit', onFormSubmit);
-buttonClosePopUp.addEventListener('click', ClosePopUp);
-buttonAddNewTask.addEventListener('click', ActivePopUp);
-catalogButton.addEventListener('click', showCatalogProducts);
+    form.addEventListener('submit', onFormSubmit);
+    buttonClosePopUp.addEventListener('click', ClosePopUp);
+    buttonAddNewTask.addEventListener('click', ActivePopUp);
+    catalogButton.addEventListener('click', showCatalogProducts);
 };
 
 init();
+
+// var tablica = [3,2,2,1,4];
+// var kopiaTablicy = tablica;
+// kopiaTablicy.sort();
